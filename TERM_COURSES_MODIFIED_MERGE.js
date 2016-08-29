afcfa0831fcf755bef75848e8254e11493d6f888
data-cat <rachelpscherer@gmail.com>
2016-08-28 20:36:12 -0400

    Initial commit. Supporting visualizations in Bb Learn using Bb DVF B2

---
+++ TERM_COURSES_MODIFIED_MERGE.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+if (#THE_COUNT# < 1){
+document.write('<span style=\"color: #666666; font-size: 10pt; font-family: arial\">No recent updates.</span>');
+} else {
+document.write('<span style=\"color: #666666; font-size: 10pt; font-family: arial\"><repeater>#MESSAGE#</repeater></span>');}
+</script>
